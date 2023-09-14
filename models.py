2b166a3e6c885c3eeffa0975a683a610216123dc
chikalahunter <chikaugwu88@gmail.com>
2023-09-14 19:33:14 +0200

    Initial commit

---
+++ models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Create a PostgreSQL database connection
+DATABASE_URL = "postgresql://your_username:your_password@localhost/your_database_name"
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+# Define the User model
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+    email_address = Column(String(255), unique=True)
+    phone_number = Column(String(20), unique=True)
+    user_name = Column(String(50), unique=True)
+    created_at = Column(DateTime, server_default='NOW()')
+    updated_at = Column(DateTime, server_default='NOW()', onupdate='NOW()')
+
+# Create the tables in the database
+Base.metadata.create_all(engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
