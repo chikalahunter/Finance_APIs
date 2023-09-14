770c2dfc295a83c99e190ad9adfe35dccc62fda6
chikalahunter <chikaugwu88@gmail.com>
2023-09-14 19:45:31 +0200

    correction

--- models.py
+++ models.py
@@ -2,9 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Create a PostgreSQL database connection
-DATABASE_URL = "postgresql://your_username:your_password@localhost/your_database_name"
-engine = create_engine(DATABASE_URL)
+engine = create_engine
 
 Base = declarative_base()
 
